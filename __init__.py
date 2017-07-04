e316d9e44ae7ebf5f8a78457557197b2cca5b34c
Ryan Gerlach <gerlachry@gmail.com>
2017-07-03 21:31:06 -0400

    token auth, clean pycharm files

--- __init__.py
+++ __init__.py
@@ -14,13 +14,9 @@ LOGGER = getLogger(__name__)
 
 
 class TodoistSkill(MycroftSkill):
-
     def __init__(self):
         super(TodoistSkill, self).__init__(name="TodoistSkill")
-        self.client_id = self.config.get('client_id')
-        self.auth_url = 'https://todoist.com/oauth/authorize'
-        self.test_token = self.config.get('test_token')
-        LOGGER.debug('client_id: {0}'.format(self.client_id))
+        self.token = self.config.get('token')
 
     def initialize(self):
         intent = IntentBuilder("TodoistIntent")\
@@ -31,29 +27,16 @@ class TodoistSkill(MycroftSkill):
         self.register_intent(intent, self.handle_intent)
 
     def handle_intent(self, message):
-        LOGGER.debug('message: {0}'.format(message.serialize()))
         task = message.data.get('Task', None)
         project = message.data.get('Project', None)
         LOGGER.debug('adding task {0} to {1} project'.format(task, project))
 
         try:
             self.speak_dialog('add.task')
-            if self.test_token is None:
-                params = {'client_id': self.client_id
-                          , 'scope': 'data:read_write'
-                          , 'state': 'supersecret'}
-                resp = requests.get(self.auth_url, params=params)
-                LOGGER.debug('auth resp: {0}'.format(resp.status_code))
-                LOGGER.debug('auth resp: {0}'.format(resp.headers))
-                LOGGER.debug('auth cookeies: {0}'.format(resp.cookies.keys))
-                LOGGER.debug('auth history: {0}'.format(resp.history))
-                LOGGER.debug('auth redirect: {0}'.format(resp.history[0].text))
-
-            api = todoist.TodoistAPI(self.test_token)
-            resp = api.sync()
+            api = todoist.TodoistAPI(self.token)
+            api.sync()
             prj_id = None
             for prj in api.projects.all():
-                LOGGER.debug(prj.data['name'] + ' : ' + str(prj.data['id']))
                 if prj['name'].lower() == project:
                     prj_id = prj['id']
                     break
@@ -63,9 +46,7 @@ class TodoistSkill(MycroftSkill):
                 LOGGER.debug('project {0} not found, please create it first in Todoist'.format(project))
                 self.speak_dialog('project.notfound')
             else:
-                LOGGER.debug('prj_id {0}'.format(prj_id))
                 item = api.add_item(content=task, project_id=prj_id)
-                LOGGER.debug('item: {0}'.format(item))
                 if 'id' in item:
                     self.speak_dialog('add.task.complete')
                 else:
